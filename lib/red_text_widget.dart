cd86b9d8d6244553d1e7ee6c72d6f1a2ca1c892e
afgan galih <afgangalihf@gmail.com>
2025-10-08 10:39:25 +0700

    step 5 - 6

--- lib/red_text_widget.dart
+++ lib/red_text_widget.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:auto_size_text/auto_size_text.dart'; // ← tambahkan ini di atas
+import 'package:auto_size_text/auto_size_text.dart';
 
 class RedTextWidget extends StatelessWidget {
-  const RedTextWidget({Key? key}) : super(key: key);
+  final String text;
+
+  const RedTextWidget({Key? key, required this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AutoSizeText(
-      text, // ← di sini muncul error dulu
+      text,
       style: const TextStyle(color: Colors.red, fontSize: 14),
       maxLines: 2,
       overflow: TextOverflow.ellipsis,
